1af26226aaf164db1c59175499bac3c03c67419a
Matthew Smeets <matthew@reuzenpanda.nl>
2025-02-26 12:31:55 +0100

    .11

--- src/main/java/io/github/matts/emailengine/model/hook/data/MessageEventData.java
+++ src/main/java/io/github/matts/emailengine/model/hook/data/MessageEventData.java
@@ -1,4 +1,10 @@
 package io.github.matts.emailengine.model.hook.data;
 
+import lombok.Data;
+
+@Data
 public class MessageEventData {
+    private String id;
+    private String emailId;
+    private String messageId;
 }
