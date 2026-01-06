7b2f6e614c31c671808f0dc937fb007ad4fceeff
Matthew Smeets <matthew@reuzenpanda.nl>
2026-01-06 17:38:26 +0100

    Fix

--- src/main/java/io/github/matts/emailengine/model/MessageReferenceAction.java
+++ src/main/java/io/github/matts/emailengine/model/MessageReferenceAction.java
@@ -1,6 +1,22 @@
 package io.github.matts.emailengine.model;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum MessageReferenceAction {
-    forward,
-    reply
+    forward("forward"),
+    reply("reply"),
+    @JsonProperty("reply-all")
+    replyAll("reply-all");
+
+    private final String value;
+
+    MessageReferenceAction(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
 }
